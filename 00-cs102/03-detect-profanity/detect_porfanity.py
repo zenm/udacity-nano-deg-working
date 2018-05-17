83fb354bf455f01bc2e33d25f3e1376d88a3a992
Lino <linojr@gmail.com>
2018-05-17 06:22:23 -0500

    Added function to detect profanity

--- 00-cs102/03-detect-profanity/detect_porfanity.py
+++ 00-cs102/03-detect-profanity/detect_porfanity.py
@@ -1,7 +1,15 @@
+import urllib
+
 def read_text():
-    quotes = open("/Users/ljimenez/Desktop/detect-profanity/movie_quotes.txt")
+    quotes = open("./movie_quotes.txt")
     contents_of_file = quotes.read()
     print(contents_of_file)
     quotes.close()
+    check_profanity(contents_of_file)
 
+def check_profanity(text_to_check):
+    connection = urllib.urlopen('http://www.wdylike.appspot.com/?q='+text_to_check)
+    output = connection.read()
+    # print(output)
+    connection.close()
 read_text()
---
+++ 00-cs102/04-using-built-in-functions.py
@@ -0,0 +1,6 @@
+# using sorted
+unsorted = [4, 5, 2, 482, 2, 99, 2094, 3018]
+print(unsorted)
+
+sorted_list = sorted(unsorted)
+print(sorted_list)
